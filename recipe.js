c202a0e2c71a0ebe35b13f1c556a7a4f7a3f902c
Vannak Chhay <vannak.chhay1@gmail.com>
2016-05-09 14:45:36 -0400

    done

--- recipe.js
+++ recipe.js
@@ -1,1 +1,25 @@
 'use strict';
+function addIngredient(recipe, ingredient, value){
+  recipe[ingredient] = value;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, value){
+  recipe[ingredient] = value;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  if(Object.keys(recipe).length == 0){
+    console.log("Recipe is empty");
+  } else {
+    for(var ingredient in recipe){
+      console.log("this recipe calls for " + recipe[ingredient] + " of " + ingredient);
+    }
+  }
+}
